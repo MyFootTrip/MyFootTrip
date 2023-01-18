d604becd1051b9a53241d8007aa54c6acd5f6ffe
SooYun Jo <josooyeon11@naver.com>
2023-01-18 13:05:05 +0900

    [FEATURE] HomeFragment 정렬 기능, 새로고침 기능 추가

--- Cli/app/src/main/java/com/app/myfoottrip/data/dto/viewmodel/BoardViewModel.kt
+++ Cli/app/src/main/java/com/app/myfoottrip/data/dto/viewmodel/BoardViewModel.kt
@@ -7,27 +7,25 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.app.myfoottrip.data.dto.Board
 import com.app.myfoottrip.data.repository.BoardRepository
+import com.app.myfoottrip.util.NetworkResult
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 private const val TAG = "싸피"
 
 class BoardViewModel : ViewModel() {
-    
+
+    private val boardRepository = BoardRepository()
+
     // 이메일 사용 여부 체크 값 livedata
-    private val _boardList = MutableLiveData<ArrayList<Board>>()
-    val boardList: LiveData<ArrayList<Board>>
-        get() = _boardList
+    val boardList: LiveData<NetworkResult<ArrayList<Board>>>
+        get() = boardRepository.boardListResponseLiveData
 
 
     // 이메일 중복 체크
     fun getBoardList() {
-        val job = viewModelScope.async {
-            BoardRepository().getBoardList()
-        }
-
         viewModelScope.launch {
-            _boardList.value = BoardRepository().getBoardList()
+            boardRepository.getBoardList()
         }
     } // End of emailUsedCheck
 
--- Cli/app/src/main/java/com/app/myfoottrip/data/repository/BoardRepository.kt
+++ Cli/app/src/main/java/com/app/myfoottrip/data/repository/BoardRepository.kt
@@ -1,10 +1,13 @@
 package com.app.myfoottrip.data.repository
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.app.myfoottrip.Application
 import com.app.myfoottrip.data.dto.Board
 import com.app.myfoottrip.network.api.UserApi
 import com.app.myfoottrip.network.service.BoardService
+import com.app.myfoottrip.util.NetworkResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -14,21 +17,24 @@ private const val TAG = "싸피"
 class BoardRepository {
     private val boardService = Application.retrofit.create(BoardService::class.java)
 
+    private val _boardListResponseLiveData = MutableLiveData<NetworkResult<ArrayList<Board>>>()
+    val boardListResponseLiveData: LiveData<NetworkResult<ArrayList<Board>>>
+        get() = _boardListResponseLiveData
+
     // 이메일 중복 체크
-    suspend fun getBoardList(): ArrayList<Board> {
-        var boardList = arrayListOf<Board>()
+    suspend fun getBoardList(){
 
-        withContext(Dispatchers.IO) {
-            var response = boardService.getBoardList()
-            Log.d(TAG, "checkEmailId: $response")
+        var response = boardService.getBoardList()
 
-            if (response.isSuccessful) {
-                boardList = response.body() as ArrayList<Board>
-            } else {
-                Log.d(TAG, "response 실패 값: ${response.errorBody()}")
-            }
+        // 처음은 Loading 상태로 지정
+        _boardListResponseLiveData.postValue(NetworkResult.Loading())
+
+        if (response.isSuccessful && response.body() != null) {
+            _boardListResponseLiveData.postValue(NetworkResult.Success(response.body()!!))
+        } else if (response.errorBody() != null) {
+            _boardListResponseLiveData.postValue(NetworkResult.Error(response.errorBody()!!.string()))
+        } else {
+            _boardListResponseLiveData.postValue(NetworkResult.Error(response.headers().toString()))
         }
-
-        return boardList
-    } // End of checkEmailId
+    }
 } // End of BoardRepository
