7e50717120279c00286d1fa1f57ef7702bf4f4b7
luminaries1 <luminaries1@naver.com>
2023-01-27 16:42:55 +0900

    BE 2023-01-27

--- BE/pjtback/community/urls.py
+++ BE/pjtback/community/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path('board/', views.board_get),
     path('board/create/', views.board_create),
     path('board/filter/', views.board_filtered),
+    path('board/detail/<int:board_id>/', views.board_detail),
     path('travel/', views.travel_get),
     path('travel/create/', views.travel_create),
-    path('traveldetail/<int:travel_id>/', views.travel_detail),
-    path('traveluser/<int:user_id>/',views.travel_user),
+    path('travel/detail/<int:travel_id>/', views.travel_detail),
+    path('travel/user/<int:user_id>/',views.travel_user),
+    path('board/like/<int:board_id>/', views.like),
 ]
