6959310849b3cf8b670481fd7d037927e5b780bf
yyyrin <qsc130@gmail.com>
2023-02-10 15:53:37 +0900

    한 개 덜 올라가서 올리는 commit

--- Cli/app/src/main/java/com/app/myfoottrip/ui/view/travel/TravelSelectFragment.kt
+++ Cli/app/src/main/java/com/app/myfoottrip/ui/view/travel/TravelSelectFragment.kt
@@ -111,6 +111,10 @@ class TravelSelectFragment : BaseFragment<FragmentTravelSelectBinding>(
             findNavController().popBackStack()
         }
 
+        binding.ivTravelHelp.setOnClickListener {
+            findNavController().navigate(R.id.action_travelSelectFragment_to_travelHelpFragment)
+        }
+
         buttonSetTextObserve()
 
         // 유저 생성 ResponseLiveData 다시 초기화
@@ -157,6 +161,7 @@ class TravelSelectFragment : BaseFragment<FragmentTravelSelectBinding>(
             binding.tvTravelTitle.setText(R.string.plz_travel_select_button_text)
             binding.btnSave.visibility = View.VISIBLE
             binding.btnSave.setText(R.string.make_new_travel_button_text)
+            binding.ivTravelHelp.visibility = View.VISIBLE
         } else if (type == 1) {
             // 만들어진 여행 관리 부분
             binding.tvTravelTitle.setText(R.string.select_travel_title)
