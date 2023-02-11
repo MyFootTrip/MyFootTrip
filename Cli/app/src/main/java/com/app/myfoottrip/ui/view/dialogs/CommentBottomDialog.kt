9e2a40fe46b955d1e854940d4e781007704a9a66
su6378 <josooyeon11@naver.com>
2023-02-11 19:44:12 +0900

    [Desing] UI 최종 수정

--- Cli/app/src/main/java/com/app/myfoottrip/ui/view/dialogs/CommentBottomDialog.kt
+++ Cli/app/src/main/java/com/app/myfoottrip/ui/view/dialogs/CommentBottomDialog.kt
@@ -1,41 +1,39 @@
-package com.app.myfoottrip.ui.view.board
+package com.app.myfoottrip.ui.view.dialogs
 
-import android.content.Context
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import androidx.activity.OnBackPressedCallback
+import android.view.*
 import androidx.appcompat.widget.PopupMenu
 import androidx.core.content.ContextCompat
 import androidx.core.view.setPadding
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
 import com.app.myfoottrip.R
 import com.app.myfoottrip.data.dto.Board
 import com.app.myfoottrip.data.dto.Comment
 import com.app.myfoottrip.data.viewmodel.BoardViewModel
 import com.app.myfoottrip.data.viewmodel.CommentViewModel
 import com.app.myfoottrip.data.viewmodel.UserViewModel
-import com.app.myfoottrip.databinding.FragmentCommentBinding
+import com.app.myfoottrip.databinding.DialogCommentBottomBinding
 import com.app.myfoottrip.ui.adapter.CommentAdapter
-import com.app.myfoottrip.ui.base.BaseFragment
-import com.app.myfoottrip.ui.view.dialogs.CommentInputDialog
-import com.app.myfoottrip.ui.view.main.MainActivity
 import com.app.myfoottrip.util.NetworkResult
-import com.app.myfoottrip.util.showSnackBarMessage
+import com.app.myfoottrip.util.showToastMessage
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.*
 
-private const val TAG = "CommentFragment_마이풋트립"
-class CommentFragment : BaseFragment<FragmentCommentBinding>(
-    FragmentCommentBinding::bind, R.layout.fragment_comment
-) {
-    private lateinit var mainActivity: MainActivity
 
+private const val TAG = "CommentBottomDialog_마이풋트립"
+class CommentBottomDialog(private val listener: OnClickListener,private val commentList: ArrayList<Comment>) :
+    BottomSheetDialogFragment(),
+    View.OnClickListener {
+
+    val mTag = "댓글 바텀 시트 다이얼로그"
+
+    private lateinit var binding: DialogCommentBottomBinding
     private val boardViewModel by activityViewModels<BoardViewModel>()
     private val userViewModel by activityViewModels<UserViewModel>()
     private val commentViewModel by activityViewModels<CommentViewModel>()
@@ -43,36 +41,42 @@ class CommentFragment : BaseFragment<FragmentCommentBinding>(
     private lateinit var commentAdapter: CommentAdapter
     private var isWrite = false
 
-    private lateinit var callback: OnBackPressedCallback
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.CustomBottomSheetDialog)
+    }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        mainActivity = context as MainActivity
-        callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                findNavController().popBackStack()
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
-    } // End of onAttach
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DialogCommentBottomBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initObserver()
 
         init()
 
         binding.apply {
-            ivBack.setOnClickListener {findNavController().popBackStack()} //뒤로가기
             clComment.setOnClickListener { initCommentInput() }
+            rvComment.isNestedScrollingEnabled = true
+            ivCancelBtn.setOnClickListener {
+                dialog?.dismiss()
+            }
         }
     }
 
-    override fun onDetach() {
-        super.onDetach()
-        callback.remove()
-    } // End of onDetach
+    interface OnClickListener {
+        fun onClick(dialog: CommentBottomDialog)
+    }
+
+    override fun onClick(p0: View?) {
+        listener.onClick(this)
+    }
 
     private fun init(){
         initProfileImg()
@@ -89,15 +93,17 @@ class CommentFragment : BaseFragment<FragmentCommentBinding>(
         binding.apply {
             if (userViewModel.wholeMyData.value?.join?.profile_image.isNullOrEmpty()){
                 ivProfile.setPadding(10)
-                Glide.with(this@CommentFragment).load(R.drawable.ic_my).fitCenter().into(ivProfile)
+                Glide.with(this@CommentBottomDialog).load(R.drawable.ic_my).fitCenter().into(ivProfile)
                 ivProfile.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(),R.color.white))
                 cvProfileLayout.setCardBackgroundColor(ColorStateList.valueOf(ContextCompat.getColor(requireContext(),R.color.main)))
             }else {
-                Glide.with(this@CommentFragment).load(userViewModel.wholeMyData.value?.join?.profile_image).centerCrop().into(ivProfile)
+                Glide.with(this@CommentBottomDialog).load(userViewModel.wholeMyData.value?.join?.profile_image).centerCrop().into(ivProfile)
                 cvProfileLayout.setCardBackgroundColor(ColorStateList.valueOf(ContextCompat.getColor(requireContext(),R.color.white)))
             }
         }
     }
+
+
 
     // 댓글이 없을 시 보여주는 뷰
     private fun initCommentExist(board : Board){
@@ -110,14 +116,14 @@ class CommentFragment : BaseFragment<FragmentCommentBinding>(
     private fun initCommentAdapter(){
 
         boardViewModel.board.value?.data!!.commentList.sortByDescending {it.writeDate}
-
+        Log.d(TAG, "테스트: ${boardViewModel.board.value?.data!!.commentList}")
         commentAdapter = CommentAdapter(boardViewModel.board.value?.data!!.commentList,userViewModel.wholeMyData.value!!.uid)
 
         commentAdapter.setItemClickListener(object : CommentAdapter.ItemClickListener {
             override fun onClick(view: View, position: Int, commentId: Int) {
                 var popupMenu = PopupMenu(requireContext(),view)
 
-                mainActivity.menuInflater.inflate(R.menu.comment_menu,popupMenu.menu)
+                requireActivity().menuInflater.inflate(R.menu.comment_menu,popupMenu.menu)
 
                 popupMenu.show()
                 popupMenu.setOnMenuItemClickListener {
@@ -132,8 +138,8 @@ class CommentFragment : BaseFragment<FragmentCommentBinding>(
                             deleteComment(commentId)
                             return@setOnMenuItemClickListener true
                         }else ->{
-                            return@setOnMenuItemClickListener true
-                        }
+                        return@setOnMenuItemClickListener true
+                    }
                     }
                 }
             }
@@ -151,7 +157,8 @@ class CommentFragment : BaseFragment<FragmentCommentBinding>(
             override fun onClick(dialog: CommentInputDialog) {
                 val user = userViewModel.wholeMyData.value
                 val message = "${user?.join?.nickname}님이 ${boardViewModel.board.value?.data?.title}에 댓글이 달렸습니다!"
-                val comment = Comment(-1, boardViewModel.boardId,user!!.join.profile_image,user.uid,user.join.nickname,dialog.commentMsg.text.toString(),Date(System.currentTimeMillis()),message)
+                val comment = Comment(-1, boardViewModel.boardId,user!!.join.profile_image,user.uid,user.join.nickname,dialog.commentMsg.text.toString(),
+                    Date(System.currentTimeMillis()),message)
                 writeComment(comment)
                 isWrite = true
                 dialog.dismiss()
@@ -166,7 +173,8 @@ class CommentFragment : BaseFragment<FragmentCommentBinding>(
         val updateDialog = CommentInputDialog(object : CommentInputDialog.OnClickListener {
             override fun onClick(dialog: CommentInputDialog) {
                 val user = userViewModel.wholeMyData.value
-                val comment = Comment(commentId, boardViewModel.boardId,user!!.join.profile_image,user.uid,user.join.nickname,dialog.commentMsg.text.toString(),Date(System.currentTimeMillis()),"")
+                val comment = Comment(commentId, boardViewModel.boardId,user!!.join.profile_image,user.uid,user.join.nickname,dialog.commentMsg.text.toString(),
+                    Date(System.currentTimeMillis()),"")
                 updateComment(comment)
                 dialog.dismiss()
             }
@@ -215,7 +223,7 @@ class CommentFragment : BaseFragment<FragmentCommentBinding>(
                     commentAdapter.notifyItemInserted(0)
                     binding.rvComment.scrollToPosition(0)
                     if (isWrite){
-                        binding.root.showSnackBarMessage("댓글이 등록되었습니다.")
+                        requireContext().showToastMessage("댓글이 등록되었습니다.")
                     }
                 }
                 is NetworkResult.Error -> {
@@ -242,7 +250,7 @@ class CommentFragment : BaseFragment<FragmentCommentBinding>(
                     initCommentExist(it.data!!)
                     initCommentAdapter()
                     commentAdapter.notifyItemChanged(position)
-                    binding.root.showSnackBarMessage("댓글이 수정되었습니다.")
+                    requireContext().showToastMessage("댓글이 수정되었습니다.")
                 }
                 is NetworkResult.Error -> {
                 }
@@ -267,7 +275,7 @@ class CommentFragment : BaseFragment<FragmentCommentBinding>(
                     initCommentExist(it.data!!)
                     initCommentAdapter()
                     commentAdapter.notifyItemRemoved(position)
-                    binding.root.showSnackBarMessage("댓글이 삭제되었습니다.")
+                    requireContext().showToastMessage("댓글이 삭제되었습니다.")
                 }
                 is NetworkResult.Error -> {}
                 is NetworkResult.Loading -> {}
